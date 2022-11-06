c127a7895cbc33e9efd5104fd8acd9c4ec6580b3
Sviatoslav10 <115168488+Sviatoslav10@users.noreply.github.com>
2022-11-06 13:58:35 +0200

    Added "Aplication start..." text

--- Lab2.1/Lab2.1/Lab2.1.cpp
+++ Lab2.1/Lab2.1/Lab2.1.cpp
@@ -10,17 +10,18 @@ int main()
 {
 	double a, b; // вхідний параметр
 	double z1; // sin(α) + cos((2β - α))/cos(α) - sin((2β - α));
-	// double z2; // 1 + sin(2β)/cos(2β);
+	double z2; // 1 + sin(2β)/cos(2β);
 
+	cout << "Application start..." << endl;
 	cout << "a = "; cin >> a;
 	cout << "b = "; cin >> b;
 
 	z1 = (sin(a) + cos(2 * b - a)) / (cos(a) - sin(2 * b - a));
-	// z2 = (1 + sin(2 * b)) / cos(2 * b);
+	z2 = (1 + sin(2 * b)) / cos(2 * b);
 
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	// cout << "z2 = " << z2 << endl;
+	cout << "z2 = " << z2 << endl;
 
 	cin.get();
 	return 0;
